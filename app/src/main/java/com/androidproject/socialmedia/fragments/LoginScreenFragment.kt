3f5f9f5005375406be8bafb13464a16aa8b3226e
Suhail <Suhail@vajro.com>
2022-08-07 12:47:49 +0530

    *Login Screen UI

---
+++ app/src/main/java/com/androidproject/socialmedia/fragments/LoginScreenFragment.kt
@@ -0,0 +1,27 @@
+package com.androidproject.socialmedia.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.androidproject.socialmedia.R
+
+
+class LoginScreenFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_screen, container, false)
+    }
+
+
+}
